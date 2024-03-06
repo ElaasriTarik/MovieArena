cee50877dea1b63f47eacaed8bfc30590fc765eb
Abdessadek EL AASRI <elaasriya2003@gmail.com>
2024-03-06 16:00:21 +0100

    PFE

---
+++ web_interface/scripts/home.js
@@ -0,0 +1,42 @@
+$(function () {
+	// Get the search input
+	const searchInput = $('#searchText');
+	searchInput.on('input', (e) => {
+		$('#searchResults').css('display', 'block');
+		$.ajax({
+			type: 'GET',
+			url: `https://api.themoviedb.org/3/search/movie?query=${e.target.value}&include_adult=false&language=en-US&page=1`,
+			headers: {
+				'Authorization': 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhYjIzNzkyOGRjYTE2MDYwNDkzN2E3MWY5NDdkNmM2MiIsInN1YiI6IjY1ZTMyNDhiNDk4ZWY5MDE2NGVjY2U4NiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.5gRke4f-AmvSdy_tRRB93Sd2TuxA3tOw_2NCRhlM3L4',
+				'accept': 'application/json',
+			},
+			success: (data) => {
+				movies = data.results;
+				console.log(movies);
+				$("#searchResults").empty();
+				for (let i = 0; i < movies.length; i++) {
+					$('#searchResults').append(`
+						<div class="col-md-3">
+							<div class="well text-center">
+								<div class="movieImageContainer">
+									<img src="https://image.tmdb.org/t/p/w500${movies[i].poster_path}" alt="Movie Poster" class="movieSearchPoster">
+								</div>
+								<div class="movieInfo">
+								<h5 class="titleSearch">${movies[i].title}</h5>
+								<div class="ratingContainer">
+								<img src="images/star.png" alt="Rating" class="SearchRatingIcon">
+								<h6 class="ratingSearch">${movies[i].vote_average.toFixed(1)}</h6>
+								</div>
+								<h5 class="releaseDateSearch">${movies[i].release_date.split('-')[0]}</h5>
+								</div>
+								<a onclick="movieSelected('${movies[i].id}')" class="btn btn-primary movieDetail" href="#"><img src="images/pngwing.com.png"</a>
+							</div>
+					`);
+				}
+			}
+		})
+	});
+	$(window).click(function () {
+		$('#searchResults').css('display', 'none');
+	});
+});
---
+++ web_interface/scripts/latests.js
@@ -0,0 +1,78 @@
+$(() => {
+	$.ajax({
+		url: "https://api.themoviedb.org/3/movie/popular?language=en-US&page=1",
+		type: 'GET',
+		headers: {
+			"accept": "application/json",
+			"Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhYjIzNzkyOGRjYTE2MDYwNDkzN2E3MWY5NDdkNmM2MiIsInN1YiI6IjY1ZTMyNDhiNDk4ZWY5MDE2NGVjY2U4NiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.5gRke4f-AmvSdy_tRRB93Sd2TuxA3tOw_2NCRhlM3L4"
+
+		},
+		success: (data) => {
+			latestMovies = data.results;
+			for (let i = 0; i < latestMovies.length; i++) {
+				$('#mainContentBody').append(`
+					<div class="movieCard">
+							<div class="movieImage">
+							<div class="ratingContainerBody">
+									<img src="images/star.png" alt="Rating" class="ratingIconBody">
+									<p class="rating">${latestMovies[i].vote_average.toFixed(1)}</p>
+								</div>
+								<img src="https://image.tmdb.org/t/p/w500${latestMovies[i].poster_path}" alt="Movie Poster" class="moviePoster">
+							</div>
+					</div>`);
+			}
+		},
+	});
+	// latest shows
+	$.ajax({
+		url: "https://api.themoviedb.org/3/trending/tv/week?language=en-US",
+		type: 'GET',
+		headers: {
+			"accept": "application/json",
+			"Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhYjIzNzkyOGRjYTE2MDYwNDkzN2E3MWY5NDdkNmM2MiIsInN1YiI6IjY1ZTMyNDhiNDk4ZWY5MDE2NGVjY2U4NiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.5gRke4f-AmvSdy_tRRB93Sd2TuxA3tOw_2NCRhlM3L4"
+
+		},
+		success: (data) => {
+			latestShows = data.results;
+			console.log(latestShows);
+			for (let i = 0; i < latestShows.length; i++) {
+				$('#latestShowsBody').append(`
+					<div class="movieCard">
+					<div class="ratingContainerBody">
+									<img src="images/star.png" alt="Rating" class="ratingIconBody">
+									<p class="rating">${latestShows[i].vote_average.toFixed(1)}</p>
+								</div>
+							<div class="movieImage" data-id="${latestShows}">
+								<img src="https://image.tmdb.org/t/p/w500${latestShows[i].poster_path}" alt="Movie Poster" class="moviePoster">
+							</div>
+					</div>`);
+			}
+		},
+	});
+	// top rated shows
+	$.ajax({
+		url: "https://api.themoviedb.org/3/tv/top_rated?language=en-US&page=1",
+		type: 'GET',
+		headers: {
+			"accept": "application/json",
+			"Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhYjIzNzkyOGRjYTE2MDYwNDkzN2E3MWY5NDdkNmM2MiIsInN1YiI6IjY1ZTMyNDhiNDk4ZWY5MDE2NGVjY2U4NiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.5gRke4f-AmvSdy_tRRB93Sd2TuxA3tOw_2NCRhlM3L4"
+
+		},
+		success: (data) => {
+			topRatedShows = data.results;
+			console.log(topRatedShows);
+			for (let i = 0; i < topRatedShows.length; i++) {
+				$('#topRatedShowsBody').append(`
+					<div class="movieCard">
+							<div class="movieImage" data-id="${topRatedShows}">
+								<div class="ratingContainerBody">
+									<img src="images/star.png" alt="Rating" class="ratingIconBody">
+									<p class="rating">${topRatedShows[i].vote_average.toFixed(1)}</p>
+								</div>
+								<img src="https://image.tmdb.org/t/p/w500${topRatedShows[i].poster_path}" alt="Movie Poster" class="moviePoster">
+							</div>
+					</div>`);
+			}
+		},
+	});
+});
