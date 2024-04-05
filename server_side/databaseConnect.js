e1e1336714794954306418cabec743198ee5697f
Abdessadek EL AASRI <elaasriya2003@gmail.com>
2024-04-05 12:51:55 +0000

    n

--- server_side/databaseConnect.js
+++ server_side/databaseConnect.js
@@ -560,6 +560,6 @@ function checkUser(username) {
 
 const port = process.env.PORT || 5000;
 
-app.listen(5500, () => {
+app.listen(port, () => {
 	console.log('Server is running on port 5500');
 });
